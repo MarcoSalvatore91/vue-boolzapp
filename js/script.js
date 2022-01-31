a48108501a9a48ded1ab66ae0436d4a01ab6eeeb
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-31 14:59:07 +0100

    added section message (incomplete)

--- js/script.js
+++ js/script.js
@@ -7,6 +7,27 @@ const message = new Vue({
     el: '#root',
 
     data: {
-    
+        user: {
+            name: 'Marco',
+            avatar: '_io'
+          },
+          contacts: [
+            {
+              name: 'Michele',
+              avatar: '_1',
+            },
+            {
+              name: 'Fabio',
+              avatar: '_2',
+            },
+            {
+              name: 'Samuele',
+              avatar: '_3',
+            },
+            {
+              name: 'Luisa',
+              avatar: '_4',
+            },
+          ]
     },
 })
