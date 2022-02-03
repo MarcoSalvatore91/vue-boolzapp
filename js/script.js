b96cc389113ed207e47ad48dbfb44aa92021a67b
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-03 15:14:21 +0100

    css menu

--- js/script.js
+++ js/script.js
@@ -14,6 +14,11 @@ const message = new Vue({
         
         currentIndex: 0,
 
+        dropDown: {
+                info: 'Info Messaggio',
+                deleteMessage: 'Cancella Messaggio',
+            },
+
         user: {
             name: 'Marco',
             avatar: '_io'
@@ -25,19 +30,22 @@ const message = new Vue({
             avatar: '_1',
             visible: true,
             messages: [{
-            date: '10/01/2020 15:30:55',
-            text: 'Hai portato a spasso il cane?',
-            status: 'sent'
+                date: '10/01/2020 15:30:55',
+                text: 'Hai portato a spasso il cane?',
+                status: 'sent',
+                menuDown: false,
             },
             {
             date: '10/01/2020 15:50:00',
             text: 'Ricordati di dargli da mangiare',
-            status: 'sent'
+            status: 'sent',
+            menuDown: false,
             },
             {
             date: '10/01/2020 16:15:22',
             text: 'Tutto fatto!',
-            status: 'received'
+            status: 'received',
+            menuDown: false,
             }
             ],
         },
@@ -48,17 +56,20 @@ const message = new Vue({
             messages: [{
             date: '20/03/2020 16:30:00',
             text: 'Ciao come stai?',
-            status: 'sent'
+            status: 'sent',
+            menuDown: false,
             },
             {
             date: '20/03/2020 16:30:55',
             text: 'Bene grazie! Stasera ci vediamo?',
-            status: 'received'
+            status: 'received',
+            menuDown: false,
             },
             {
             date: '20/03/2020 16:35:00',
             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-            status: 'sent'
+            status: 'sent',
+            menuDown: false,
             }
             ],
         },
@@ -69,17 +80,20 @@ const message = new Vue({
             messages: [{
             date: '28/03/2020 10:10:40',
             text: 'La Marianna va in campagna',
-            status: 'received'
+            status: 'received',
+            menuDown: false,
             },
             {
             date: '28/03/2020 10:20:10',
             text: 'Sicuro di non aver sbagliato chat?',
-            status: 'sent'
+            status: 'sent',
+            menuDown: false,
             },
             {
             date: '28/03/2020 16:15:22',
             text: 'Ah scusa!',
-            status: 'received'
+            status: 'received',
+            menuDown: false,
             }
             ],
         },
@@ -90,12 +104,14 @@ const message = new Vue({
             messages: [{
             date: '10/01/2020 15:30:55',
             text: 'Lo sai che ha aperto una nuova pizzeria?',
-            status: 'sent'
+            status: 'sent',
+            menuDown: false,
             },
             {
             date: '10/01/2020 15:50:00',
             text: 'Si, ma preferirei andare al cinema',
-            status: 'received'
+            status: 'received',
+            menuDown: false,
             }
             ],
         },
@@ -135,6 +151,12 @@ const message = new Vue({
                     contact.visible = false;
                 } else contact.visible = true;
             })
+        },
+
+        hiddenMenu () {
+            if(this.menuDown !== true){
+                return true
+            }
         },
     },
  })
